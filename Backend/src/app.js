a05ab2cd3a04abdceb27682466f94ca9931ecfa8
AnaghRoy <royanagh1999@gmail.com>
2026-03-05 15:26:15 +0530

    minor changes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,7 +4,12 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", process.env.FRONTEND_URL],
+    credentials: true,
+  }),
+);
 app.use(cookieParser());
 
 /**Routes */
--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,8 +1,8 @@
-import axios from "axios"
+import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api",
-    withCredentials: true, // For cookie support
-})
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000/api",
+  withCredentials: true, // For cookie support
+});
 
-export default axiosInstance
+export default axiosInstance;
